c6354e539b648715f65c9ae2307be6b994ae5c1d
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-11-25 15:29:44 -0300

    feat: configura a API mínima necessária como a 26

--- app/src/main/java/br/com/alura/financialcontrol/ui/activity/ExtractActivity.kt
+++ app/src/main/java/br/com/alura/financialcontrol/ui/activity/ExtractActivity.kt
@@ -1,8 +1,6 @@
 package br.com.alura.financialcontrol.ui.activity
 
-import android.os.Build
 import android.os.Bundle
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.financialcontrol.databinding.ExtractActivityBinding
 import br.com.alura.financialcontrol.model.Transaction
@@ -12,7 +10,6 @@ import java.time.LocalDate
 
 class ExtractActivity : AppCompatActivity() {
 
-    @RequiresApi(Build.VERSION_CODES.O)
     private val transactions = mutableListOf<Transaction>(
         Transaction("Salário", BigDecimal("1580.78"), "", LocalDate.now(), "credit"),
         Transaction("Luz", BigDecimal("147.41"), "", LocalDate.now(), "expense"),
@@ -46,21 +43,18 @@ class ExtractActivity : AppCompatActivity() {
         Transaction("Vale", BigDecimal("1200.77"), "", LocalDate.now(), "credit"),
     )
 
-    @RequiresApi(Build.VERSION_CODES.O)
     private val extractRecyclerViewAdapter =
         ExtractListAdapter(context = this, transactions = transactions)
     private val binding by lazy {
         ExtractActivityBinding.inflate(layoutInflater)
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(this.binding.root)
         configRecyclerViewExtract()
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     private fun configRecyclerViewExtract() {
         val recyclerView = this.binding.extractListRecyclerView
         recyclerView.adapter = this.extractRecyclerViewAdapter
